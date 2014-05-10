e8e78d741740413adcac18c30cfbd74beaf39aef
Vivek Divekar <vdivekar2013@gmail.com>
2014-05-10 23:01:25 +0530

    Added initial version of CSV File implementation

---
+++ Headingley/src/workbook/io/CSVFile.java
@@ -0,0 +1,55 @@
+package workbook.io;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import workbook.error.StorageException;
+import workbook.io.iface.File;
+
+public class CSVFile implements File {
+
+	@Override
+	public void load(String fileName) throws StorageException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load(InputStream inStream) throws StorageException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load(InputStreamReader inReader) throws StorageException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(String fileName) throws StorageException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(OutputStream outStream) throws StorageException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(OutputStreamWriter outWriter) throws StorageException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
